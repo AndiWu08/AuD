35a8311569521b8491c838bd321651d8df49da79
Andreas Würzer <awuerzer@stud.hs-heilbronn.de>
2025-04-16 17:00:31 +0200

    ex 8

---
+++ 03_8_harmonische_reihe.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+double h_r(int n) {
+    if (n <= 0)
+    {
+        return 0.0;
+    }
+    
+    return (1.0 / n) + h_r(n-1);
+}
+
+double h_i(int n) {
+    double result = 0.0;
+
+    for (int i = n; i > 0; i--)
+    {
+        result += 1.0 / i;
+    }
+    
+
+    return result;
+}
+
+int main() {
+    std::cout << h_i(3) << std::endl;
+    std::cout << h_r(3) << std::endl;
+}
